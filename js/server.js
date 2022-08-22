ef7c388fed12726ff3443a558ea3da7c32703fe1
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-08-21 20:17:42 -0700

    initial commit

---
+++ js/server.js
@@ -0,0 +1,36 @@
+const http = require('http'), //creates a variable called http, then assigns to it an instance of the HTTP module. This is what imports the HTTP module and allows you to use its function createServer().
+  fs = require('fs'),
+  url = require('url');
+
+http.createServer((request, response) => { // the two arguments inside the inner-most function will be called every time a request is made against the sever (name:request handler)
+  let addr = request.url,
+    q = url.parse(addr, true),
+    filePath = '';
+
+  fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Added to log.');
+    }
+  });
+
+  if (q.pathname.includes('documentation')) {
+    filePath = (__dirname + '/documentation.html');
+  } else {
+    filePath = 'index.html';
+  }
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      throw err;
+    }
+
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write(data);
+    response.end();
+
+  });
+
+}).listen(8080);
+console.log('My test server is running on Port 8080.');
