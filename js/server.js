05dbbabe656e5d1d1cd9ee601b31ee0a4db0e75e
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-08-22 19:53:09 -0700

    added lang to doc.html

--- js/server.js
+++ js/server.js
@@ -1,7 +1,6 @@
-const http = require('http'); //creates a variable called http, then assigns to it an instance of the HTTP module. This is what imports the HTTP module and allows you to use its function createServer().
-const fs = require('fs');
 const url = require('url');
-
+const fs = require('fs');
+const http = require('http'); //creates a variable called http, then assigns to it an instance of the HTTP module. This is what imports the HTTP module and allows you to use its function createServer().
 http.createServer((request, response) => { // the two arguments inside the inner-most function will be called every time a request is made against the sever (name:request handler)
   let addr = request.url,
     q = url.parse(addr, true),
@@ -20,6 +19,7 @@ http.createServer((request, response) => { // the two arguments inside the inner
     response.writeHead(200, { 'Content-Type': 'text/html' });
     response.write(data);
     response.end();
+
   });
 
   fs.appendFile('log.txt', 'URL: ' + addr + '\nTimestamp: ' + new Date() + '\n\n', (err) => {
