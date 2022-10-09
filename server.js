edb461305e7c60feeeef3c8d2d307be030b8d848
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-10-08 19:39:45 -0700

    updated users

--- server.js
+++ server.js
@@ -20,6 +20,16 @@ let users = [
     name: "Melissa",
     favoriteMovies: ["Scream 2"]
   },
+  {
+    id: 3,
+    name: "Dawn",
+    favoriteMovies: []
+  },
+  {
+    id: 4,
+    name: "Keith",
+    favoriteMovies: []
+  },
 ];
 
 let movies = [
@@ -28,13 +38,13 @@ let movies = [
     Title: 'Good Will Hunting',
     Story:'Will Hunting, a janitor at MIT, has a gift for mathematics but needs help from a psychologist to find direction in his life.',
     Genre: {
-      Name: "Drama, Romance",
+      Name: 'Drama',
       Description: "In film and television, drama is a category or genre of narrative fiction (or semi-fiction) intended to be more serious than humorous in tone."
     },
     Director: {
       Name: 'Gus Van Sant',
       Bio: 'Gus Green Van Sant Jr. is an American filmmaker, painter, screenwriter, photographer and musician from Louisville, Kentucky who is known for directing films such as Good Will Hunting, the 1998 remake of Psycho, Gerry, Elephant, My Own Private Idaho, To Die For, Milk, Last Days, Finding Forrester, Promised Land, Drugstore Cowboy and Mala Noche.',
-      DOB: 'July 24, 1952'
+      dob: 'July 24, 1952'
     },
     Stars: 'Matt Damon, Robin Williams, Ben Affleck, Stellan Skarsgård, Minnie Driver',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -50,7 +60,7 @@ let movies = [
     Director: {
       Name: 'Rob Reiner',
       Bio: 'Robert Reiner was born in New York City, to Estelle Reiner (née Lebost) and Emmy-winning actor, comedian, writer, and producer Carl Reiner. As a child, his father was his role model, as Carl Reiner created and starred in The Dick Van Dyke Show.',
-      DOB: 'March 6, 1947'
+      dob: 'March 6, 1947'
     },
     Stars: 'Tom Cruise, Jack Nicholson, Demi Moore, Kevin Bacon, Kiefer Sutherland',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BMmRlZDQ1MmUtMzE2Yi00YTkxLTk1MGMtYmIyYWQwODcxYzRlXkEyXkFqcGdeQXVyNTI4MjkwNjA@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -60,13 +70,13 @@ let movies = [
     Title: 'My Cousin Vinny',
     Story:'Two New Yorkers accused of murder in rural Alabama while on their way back to college call in the help of one of their cousins, a loudmouth lawyer with no trial experience.',
     Genre: {
-      Name: 'Comedy, Crime',
+      Name: 'Comedy',
       Description: 'Comedy is a genre of fiction that consists of discourses or works intended to be humorous or amusing by inducing laughter, especially in theatre, film, stand-up comedy, television, radio, books, or any other entertainment medium.'
     },
     Director: {
       Name: 'Johnathan Lynn',
       Bio: 'Jonathan Lynn and Antony Jay created and wrote every episode of the acclaimed BBC political comedy series Yes Minister (1980) and Yes, Prime Minister (1986).',
-      DOB: 'April 3, 1943'
+      dob: 'April 3, 1943'
     },  
     Stars: 'Joe Pesci, Marisa Tomei, Ralph Macchio',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BMTQxNDYzMTg1M15BMl5BanBnXkFtZTgwNzk4MDgxMTE@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -76,13 +86,13 @@ let movies = [
     Title: 'Cast Away',
     Story:'A FedEx executive undergoes a physical and emotional transformation after crash landing on a deserted island.',
     Genre: {
-      Name: 'Adventure, Drama, Romance',
+      Name: 'Adventure',
       Description: 'The adventure genre consists of books where the protagonist goes on an epic journey, either personally or geographically.'
     },
     Director: {
       Name: 'Robert Zemeckis',
       Bio: 'A whiz-kid with special effects, Robert is from the Spielberg camp of film-making (Steven Spielberg produced many of his films). Usually working with writing partner Bob Gale, Robert"s earlier films show he has a talent for zany comedy (Romancing the Stone (1984), 1941 (1979)) and special effect vehicles (Who Framed Roger Rabbit (1988) and Back to the Future (1985)). His later films have become more serious, with the hugely successful Tom Hanks vehicle Forrest Gump (1994) and the Jodie Foster film Contact (1997), both critically acclaimed movies.',
-      DOB: 'May 14, 1951'
+      dob: 'May 14, 1951'
     },  
     Stars: 'Tom Hanks, Helen Hunt, Paul Sanchez',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BN2Y5ZTU4YjctMDRmMC00MTg4LWE1M2MtMjk4MzVmOTE4YjkzXkEyXkFqcGdeQXVyNTc1NTQxODI@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -97,7 +107,7 @@ let movies = [
     Director: {
       Name: 'Wes Craven',
       Bio: 'Wesley Earl Craven was born in Cleveland, Ohio, to Caroline (Miller) and Paul Eugene Craven. He had a midwestern suburban upbringing. His first feature film was The Last House on the Left (1972), which he wrote, directed, and edited. Craven reinvented the youth horror genre again in 1984 with the classic A Nightmare on Elm Street (1984), a film he wrote and directed.',
-      DOB: 'August 2, 1939'
+      dob: 'August 2, 1939'
     },
     Stars: 'Heather Langenkamp, Johnny Depp, Robert Englund',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BNzFjZmM1ODgtMDBkMS00NWFlLTg2YmUtZjc3ZTgxMjE1OTI2L2ltYWdlXkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -112,7 +122,7 @@ let movies = [
     Director: {
       Name: 'Wes Craven',
       Bio: 'Wesley Earl Craven was born in Cleveland, Ohio, to Caroline (Miller) and Paul Eugene Craven. He had a midwestern suburban upbringing. His first feature film was The Last House on the Left (1972), which he wrote, directed, and edited. Craven reinvented the youth horror genre again in 1984 with the classic A Nightmare on Elm Street (1984), a film he wrote and directed.',
-      DOB: 'August 2, 1939'
+      dob: 'August 2, 1939'
     },
     Stars: 'Neve Campbell, Courteney Cox, David Arquette',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BMTIxNTMzNzYtNzA3NC00MzgwLTlhNGYtMDEyYTNlZjcwZTNiXkEyXkFqcGdeQXVyNDAxNjkxNjQ@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -127,7 +137,7 @@ let movies = [
     Director: {
       Name: 'Ron Howard',
       Bio: 'Academy Award-winning filmmaker Ron Howard is one of this generation"s most popular directors. From the critically acclaimed dramas A Beautiful Mind (2001) and Apollo 13 (1995) to the hit comedies Parenthood (1989) and Splash (1983), he has created some of Hollywood"s most memorable films.',
-      DOB: 'March 1, 1954'
+      dob: 'March 1, 1954'
     },
     Stars: 'Russel Crowe, Ed Harris, Jennifer Connelly',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BMzcwYWFkYzktZjAzNC00OGY1LWI4YTgtNzc5MzVjMDVmNjY0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -142,7 +152,7 @@ let movies = [
     Director: {
       Name: 'Boaz Yakin',
       Bio: 'Boaz Yakin was born in 1966 in New York City, New York, USA. He is a writer and producer, known for Fresh (1994), Remember the Titans (2000) and The Harder They Fall (2021).',
-      DOB: 'June 20, 1966'
+      dob: 'June 20, 1966'
     },
     Stars: 'Denzel Washington, Will Patton, Wood Harris',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BYThkMzgxNjEtMzFiOC00MTI0LWI5MDItNDVmYjA4NzY5MDQ2L2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_UX67_CR0,0,67,98_AL_.jpg'
@@ -151,28 +161,28 @@ let movies = [
     Title: 'Jurassic Park',
     Story:"A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose",
     Genre: {
-      Name: 'Action, Adventure, Sci-Fi',
+      Name: 'Action',
       Description: 'Action film is a film genre in which the protagonist is thrust into a series of events that typically involve violence and physical feats.'
     },
     Director: {
       Name: 'Steven Speilberg',
       Bio: 'One of the most influential personalities in the history of cinema, Steven Spielberg is Hollywood"s best known director and one of the wealthiest filmmakers in the world. He has an extraordinary number of commercially successful and critically acclaimed credits to his name, either as a director, producer or writer since launching the summer blockbuster with Jaws (1975), and he has done more to define popular film-making since the mid-1970s than anyone else.',
-      DOB: 'December 18, 1946'
+      dob: 'December 18, 1946'
     },
     Stars: 'Sam Neill, Laura Dern, Jeff Goldblum',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BMjM2MDgxMDg0Nl5BMl5BanBnXkFtZTgwNTM2OTM5NDE@._V1_UX67_CR0,0,67,98_AL_.jpg'
   },
   {
     Title: 'Avatar',
-    Story:'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.    ',
+    Story:'A paraplegic Marine dispatched to the moon Pandora on a unique mission becomes torn between following his orders and protecting the world he feels is his home.',
     Genre: {
-      Name: '',
-      Description: ''
+      Name: 'Fantasy',
+      Description: 'Fantasy fiction is a genre of writing in which the plot could not happen in real life (as we know it, at least).'
     },
     Director: {
       Name: 'James Cameron',
       Bio: 'James Francis Cameron was born on August 16, 1954 in Kapuskasing, Ontario, Canada. He moved to the United States in 1971. The son of an engineer, he majored in physics at California State University before switching to English, and eventually dropping out. He then drove a truck to support his screenwriting ambition. James Cameron is now one of the most sought-after directors in Hollywood.',
-      DOB: 'August 16, 1954'
+      dob: 'August 16, 1954'
     },
     Stars: 'Sam Worthington, Zoe Saldana, Siguorney Weaver',
     ImgURL: 'https://m.media-amazon.com/images/M/MV5BNjA3NGExZDktNDlhZC00NjYyLTgwNmUtZWUzMDYwMTZjZWUyXkEyXkFqcGdeQXVyMTU1MDM3NDk0._V1_UX67_CR0,0,67,98_AL_.jpg'
