a0f3f470ed21f47831afa855eb52f1d587746edf
JNicholas54 <102398723+JNicholas54@users.noreply.github.com>
2022-10-31 18:28:58 -0700

    adding cors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(bodyParser.urlencoded({ extended: true })); //support parsing of applica
 
 
 // Authentication
-require('./auth')(app);
+const auth = require('./auth')(app);
 
 // Passport
 const passport = require('passport');
